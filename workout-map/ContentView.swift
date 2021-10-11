43fd33ce8e08e1660754da8d8360ad7781b2765a
Josh Lalonde <mori.ryuuji3@gmail.com>
2021-10-11 14:10:24 -0300

    Initial Commit

---
+++ workout-map/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  workout-map
+//
+//  Created by Josh Lalonde on 2021-10-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ workout-map/workout_mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  workout_mapApp.swift
+//  workout-map
+//
+//  Created by Josh Lalonde on 2021-10-11.
+//
+
+import SwiftUI
+
+@main
+struct workout_mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
